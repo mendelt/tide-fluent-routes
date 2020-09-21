e7e1db484579ea4916f90ba65da4ba177f909416
Mendelt Siebenga <msiebenga@gmail.com>
2020-09-21 22:17:33 +0200

    sketch out route builder api

--- src/router.rs
+++ src/router.rs
@@ -1,11 +1,31 @@
+use std::collections::HashMap;
 use tide::{Endpoint, http::Method};
 
 pub trait Router<State: Clone + Send + Sync + 'static> {
     fn register_endpoint(&mut self, path: &str, method: Method, endpoint: impl Endpoint<State>);
+
+    fn route(&mut self, route: impl Fn(RouteBuilder<State>) -> RouteBuilder<State>) {
+        todo!()
+    }
 }
 
 impl<State: Clone + Send + Sync + 'static> Router<State> for tide::Server<State> {
     fn register_endpoint(&mut self, path: &str, method: Method, endpoint: impl Endpoint<State>) {
         self.at(path).method(method, endpoint);
+    }
+}
+
+pub struct RouteBuilder<State> {
+    path: String,
+    endpoints: HashMap<Method, Box<dyn Endpoint<State>>>,
+}
+
+impl<State: Clone + Send + Sync + 'static> RouteBuilder<State> {
+    pub fn at(self, path: &str, subroute: impl Fn(RouteBuilder<State>) -> RouteBuilder<State>) -> RouteBuilder<State> {
+        todo!()
+    }
+
+    pub fn method(self, path: &str, method: Method, endpoint: impl Endpoint<State>) -> RouteBuilder<State> {
+        todo!()
     }
 }
