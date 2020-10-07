6789c2ba182c1e4e6f37d021d2554f74f4af99fc
Mendelt Siebenga <msiebenga@gmail.com>
2020-10-07 16:41:03 +0200

    make router work on builder trait instead of of the concrete type

--- src/router.rs
+++ src/router.rs
@@ -1,7 +1,7 @@
 //! The router trait and its implementation on tide::Server connect the RouteBuilder to tide and
 //! allows you to call register on a tide::Server with a fluent route tree
 
-use crate::{EndpointDescriptor, RouteSegment};
+use crate::{EndpointDescriptor, RouteSegment, routebuilder::RouteBuilder};
 use tide::{http::Method, Endpoint};
 
 /// A router is any component where routes can be registered on like a tide::Server
@@ -15,8 +15,8 @@ pub trait Router<State: Clone + Send + Sync + 'static> {
     );
 
     /// Register all routes from a RouteBuilder on the `Router`
-    fn register(&mut self, routes: RouteSegment<State>) {
-        for EndpointDescriptor(path, method, _middleware, endpoint) in routes.build() {
+    fn register<T: RouteBuilder<State>>(&mut self, builder: RouteSegment<State>) {
+        for EndpointDescriptor(path, method, _middleware, endpoint) in builder.build() {
             self.register_endpoint(&path, method, endpoint)
         }
     }
