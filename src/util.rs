37fd8160be95d8718d82891021b77f91ba06a105
Mendelt Siebenga <msiebenga@gmail.com>
2020-10-10 22:05:42 +0200

    add ArcMiddleware wrapper

--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,12 @@
-use std::fmt::{Debug, Formatter, Result as FmtResult};
-use tide::utils::async_trait;
+use std::{
+    fmt::{Debug, Formatter, Result as FmtResult},
+    sync::Arc,
+};
 use tide::Endpoint;
+use tide::{utils::async_trait, Middleware};
 
+/// Implement some useful stuff around Box<dyn Endpoint>
 pub(crate) struct BoxedEndpoint<State>(Box<dyn Endpoint<State>>);
-
-impl<State> Debug for BoxedEndpoint<State> {
-    fn fmt(&self, formatter: &mut Formatter<'_>) -> FmtResult {
-        formatter.debug_struct("BoxedEndpoint").finish()
-    }
-}
 
 impl<State: Clone + Send + Sync + 'static> BoxedEndpoint<State> {
     /// Wrap an endpoint in a BoxedEndpoint
@@ -17,9 +15,42 @@ impl<State: Clone + Send + Sync + 'static> BoxedEndpoint<State> {
     }
 }
 
+impl<State> Debug for BoxedEndpoint<State> {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> FmtResult {
+        formatter.debug_struct("BoxedEndpoint").finish()
+    }
+}
+
 #[async_trait]
 impl<State: Clone + Send + Sync + 'static> Endpoint<State> for BoxedEndpoint<State> {
     async fn call(&self, req: tide::Request<State>) -> tide::Result {
         self.0.call(req).await
+    }
+}
+
+/// Implement some useful stuff around Arc<dyn Middleware>
+pub(crate) struct ArcMiddleware<State>(Arc<dyn Middleware<State>>);
+
+impl<State: Clone + Send + Sync + 'static> ArcMiddleware<State> {
+    /// Wrap an endpoint in a BoxedEndpoint
+    pub(crate) fn new(ware: impl Middleware<State>) -> Self {
+        Self(Arc::new(ware))
+    }
+}
+
+impl<State> Debug for ArcMiddleware<State> {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> FmtResult {
+        formatter.debug_struct("ArcMiddleware").finish()
+    }
+}
+
+#[async_trait]
+impl<State: Clone + Send + Sync + 'static> Middleware<State> for ArcMiddleware<State> {
+    async fn handle(
+        &self,
+        request: tide::Request<State>,
+        next: tide::Next<'_, State>,
+    ) -> tide::Result {
+        self.0.handle(request, next).await
     }
 }
