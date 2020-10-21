0e1fee574bc2b51d2f2da93a052bd47793a7ae2a
Mendelt Siebenga <msiebenga@gmail.com>
2020-10-21 14:34:06 +0200

    clean up fs code a bit

--- src/fs.rs
+++ src/fs.rs
@@ -5,7 +5,7 @@ use async_std::path::PathBuf as AsyncPathBuf;
 use log;
 use std::ffi::OsStr;
 use std::io;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use tide::Body;
 use tide::Response;
 use tide::{utils::async_trait, Endpoint};
@@ -15,7 +15,7 @@ use tide::{Request, Result, StatusCode};
 pub trait ServeFs<State: Clone + Send + Sync + 'static>: RouteBuilder<State> {
     /// Serve a directory at a location
     fn serve_dir(self, dir: impl AsRef<Path>) -> io::Result<Self> {
-        let dir_path = dir.as_ref().to_owned().canonicalize()?;
+        let dir_path = AsyncPathBuf::from(dir.as_ref().to_owned().canonicalize()?);
 
         Ok(self.at("*path", |route| {
             route.get(ServeDirEndpoint {
@@ -27,7 +27,7 @@ pub trait ServeFs<State: Clone + Send + Sync + 'static>: RouteBuilder<State> {
 
     /// Same as serve_dir, but for a single file
     fn serve_file(self, file: impl AsRef<Path>) -> io::Result<Self> {
-        let file_path = file.as_ref().to_owned().canonicalize()?;
+        let file_path = AsyncPathBuf::from(file.as_ref().to_owned().canonicalize()?);
 
         Ok(self.get(ServeFileEndpoint { file_path }))
     }
@@ -36,7 +36,7 @@ pub trait ServeFs<State: Clone + Send + Sync + 'static>: RouteBuilder<State> {
 impl<State: Clone + Send + Sync + 'static, R: RouteBuilder<State>> ServeFs<State> for R {}
 
 struct ServeDirEndpoint {
-    dir_path: PathBuf,
+    dir_path: AsyncPathBuf,
     prefix: String,
 }
 
@@ -45,53 +45,48 @@ impl<State: Clone + Send + Sync + 'static> Endpoint<State> for ServeDirEndpoint 
     async fn call(&self, req: Request<State>) -> Result {
         let path = req.param(&self.prefix)?.trim_start_matches('/');
 
-        let mut dir_path = self.dir_path.clone();
+        let mut file_path = self.dir_path.clone();
         for p in Path::new(path) {
             if p == OsStr::new(".") {
                 continue;
             } else if p == OsStr::new("..") {
-                dir_path.pop();
+                file_path.pop();
             } else {
-                dir_path.push(&p);
+                file_path.push(&p);
             }
         }
 
-        log::info!("Requested file: {:?}", dir_path);
+        log::info!("Requested file: {:?}", file_path);
 
-        let file_path = AsyncPathBuf::from(dir_path);
         if !file_path.starts_with(&self.dir_path) {
             log::warn!("Unauthorized attempt to read: {:?}", file_path);
-            return Ok(Response::new(StatusCode::Forbidden));
-        }
-        if !file_path.exists().await {
+            Ok(Response::new(StatusCode::Forbidden))
+        } else if !file_path.exists().await {
             log::warn!("File not found: {:?}", file_path);
-            return Ok(Response::new(StatusCode::NotFound));
+            Ok(Response::new(StatusCode::NotFound))
+        } else {
+            Ok(Response::builder(StatusCode::Ok)
+                .body(Body::from_file(&file_path).await?)
+                .build())
         }
-        let body = Body::from_file(&file_path).await?;
-        let mut res = Response::new(StatusCode::Ok);
-        res.set_body(body);
-        Ok(res)
     }
 }
 
 /// Endpoint method for serving files, path is the path to the file to serve
 struct ServeFileEndpoint {
-    file_path: PathBuf,
+    file_path: AsyncPathBuf,
 }
 
 #[async_trait]
 impl<State: Clone + Send + Sync + 'static> Endpoint<State> for ServeFileEndpoint {
     async fn call(&self, _req: Request<State>) -> Result {
-        let file_path = AsyncPathBuf::from(self.file_path.clone());
-
-        if !file_path.exists().await {
+        if !self.file_path.exists().await {
             log::warn!("File not found: {:?}", self.file_path);
             Ok(Response::new(StatusCode::NotFound))
         } else {
-            let body = Body::from_file(&file_path).await?;
-            let mut res = Response::new(StatusCode::Ok);
-            res.set_body(body);
-            Ok(res)
+            Ok(Response::builder(StatusCode::Ok)
+                .body(Body::from_file(&self.file_path).await?)
+                .build())
         }
     }
 }
